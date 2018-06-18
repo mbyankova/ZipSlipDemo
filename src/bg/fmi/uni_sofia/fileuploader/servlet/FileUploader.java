d2b90255b56d3fd7fbb83c07feb9e88b84e127c7
Mila Byankova <m.byankova@gmail.com>
2018-06-18 22:44:26 +0300

    create a web project with a servlet move Unzipper class to different package

---
+++ src/bg/fmi/uni_sofia/fileuploader/servlet/FileUploader.java
@@ -0,0 +1,68 @@
+package bg.fmi.uni_sofia.fileuploader.servlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import bg.fmi.uni_sofia.fileuploader.unzipper.Unzipper;
+
+/**
+ * Servlet implementation class FileUploader
+ */
+@WebServlet("/FileUploader")
+@MultipartConfig
+public class FileUploader extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FileUploader() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<form action='/ZipSlipDemo/FileUploader' method='post' enctype='multipart/form-data'><input type='file' name='zipFile' /><button type='submit' value='Submit!'/></form>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Part filePart = request.getPart("zipFile");
+		InputStream input = filePart.getInputStream();
+		File targetFile = new File("D:\\mila\\NS2\\ZipSlipDemo\\zipped\\" + filePart.getSubmittedFileName());
+		targetFile.createNewFile();
+		OutputStream out = new FileOutputStream(targetFile);
+		
+		byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while((bytesRead = input.read(buffer)) !=-1){
+            out.write(buffer, 0, bytesRead);
+        }
+        
+        input.close();
+        out.flush();
+        out.close();
+		
+        Unzipper.unzipFile(targetFile.getAbsolutePath());
+        
+		doGet(request, response);
+	}
+
+}
