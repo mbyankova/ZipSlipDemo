dd329353ffa34ee59dc0a8db7fad8b8e30c5313b
Mila Byankova <m.byankova@gmail.com>
2018-06-20 12:36:49 +0300

    change Unzipper to unzip in separate folders named as the zip file

--- src/bg/fmi/uni_sofia/fileuploader/unzipper/Unzipper.java
+++ src/bg/fmi/uni_sofia/fileuploader/unzipper/Unzipper.java
@@ -3,24 +3,32 @@ package bg.fmi.uni_sofia.fileuploader.unzipper;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Unzipper {
 
-	public static void unzipFile(String zipFileName, String unzipDestination) throws IOException {
-		if(!doesFileExists(zipFileName)) {
-			throw new FileNotFoundException("The given zip file not found: " + zipFileName);
+	public static void unzipFile(String zipFileWithAbsolutePath, String destination) throws IOException {
+		if(!doesFileExists(zipFileWithAbsolutePath)) {
+			throw new FileNotFoundException("The given zip file not found: " + zipFileWithAbsolutePath);
 		}
 		
-		isFilenameValid(zipFileName);
+		isFilenameValid(zipFileWithAbsolutePath);
+		
+		String fileName = getFileFromPath(zipFileWithAbsolutePath);
+		String finalDestination = createDirectoryNamedAsZipFile(fileName, destination);
 		
 		try {
 			// Initiate ZipFile object with the path/name of the zip file.
-			ZipFile zipFile = new ZipFile(zipFileName);
+			ZipFile zipFile = new ZipFile(zipFileWithAbsolutePath);
 			
 			// Extracts all files to the path specified
-			zipFile.extractAll(unzipDestination);
+			zipFile.extractAll(finalDestination);
 			
 		} catch (ZipException e) {
 			e.printStackTrace();
@@ -36,6 +44,28 @@ public class Unzipper {
 	private static boolean doesFileExists(String fileName) {
 		File f = new File(fileName);
 		return f.exists();
+	}
+	
+	private static String getFileFromPath(String path) {
+		File fileWithPath = new File(path);
+		return fileWithPath.getName();
+	}
+	
+	private static String createDirectoryNamedAsZipFile(String zipFile, String destination) {
+		FileSystem fileSystem = FileSystems.getDefault();
+
+		String targetDirectory = zipFile.replaceFirst("[.][^.]+$", "");
+		String finalDestination = destination + targetDirectory;
+		
+		try {
+			Files.createDirectory(fileSystem.getPath(finalDestination));
+		} catch (FileAlreadyExistsException e) {
+			System.out.println("Directory " + targetDirectory + " already exists");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return finalDestination;
 	}
 
 }
